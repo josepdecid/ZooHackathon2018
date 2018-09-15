ca25277ae6f42e40742229945e19d68fd2f72b8c
Gigi <giselaruzafamado@gmail.com>
2018-09-15 13:33:51 +0200

    Add endpoints for get and delete posts

--- API/app.py
+++ API/app.py
@@ -1,21 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 @app.route('/posts')
 def get_posts():
-    return 'Title:'
+    return jsonify(
+        id='321',
+        title='vendo cocodrilo',
+        adress='Inlab B5',
+        latitude='41.3884966',
+        longitude='41.3884966'
+    )
 
-@app.route('/delete',methods=['POST'])
-def delete():
-    id = request.form['id']
-    return 'Hola  se ha borrado el anuncio con id' + id
+@app.route('/posts/<id>', methods=['DELETE'])
+def delete(id):
+    return 'Hola  se ha borrado el anuncio con id ' + id
+
 
 if __name__ == '__main__':
     app.run()
