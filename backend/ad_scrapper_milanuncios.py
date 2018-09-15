da8dfc0340060a76383b8a9dcdd38c91610af8c1
FRANCESC DE PUIG <francescdepuig@gmail.com>
2018-09-15 16:45:49 +0200

    works

--- backend/ad_scrapper_milanuncios.py
+++ backend/ad_scrapper_milanuncios.py
@@ -4,20 +4,21 @@ from scrapy.crawler import CrawlerProcess
 
 
 class AdCrawler(scrapy.Spider):
-    name = 'TodoColección'
-    base_url = 'https://www.todocoleccion.net/app/buscador?O=mas&bu='
+    name = 'milanuncios'
+    #base_url = 'https://www.todocoleccion.net/app/buscador?O=mas&bu='
+    base_url = 'https://www.milanuncios.com/anuncios/'
     search_keywords = ['rinoceronte', 'marfil', 'cuerno']
 
     def start_requests(self):
-        urls = [self.base_url + keyword for keyword in self.search_keywords]
+        urls = [self.base_url + keyword +'.htm' for keyword in self.search_keywords]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         extraction = Selector(response=response, type='html')\
-            .xpath('//div[re:test(@class, "lote-list")]').extract()
+            .xpath('//div[re:test(@class, "aditem")]').extract()
 
-        with open('test.html', 'a') as f:
+        with open('test_milanuncios.html', 'a') as f:
             for el in extraction:
                 f.write(el)
 
--- backend/ad_scrapper_wallapop.py
+++ backend/ad_scrapper_wallapop.py
@@ -6,19 +6,19 @@ from scrapy.crawler import CrawlerProcess
 class AdCrawler(scrapy.Spider):
     name = 'milanuncios'
     #base_url = 'https://www.todocoleccion.net/app/buscador?O=mas&bu='
-    base_url = 'https://www.milanuncios.com/anuncios/'
+    base_url = 'https://es.wallapop.com/search?kws='
     search_keywords = ['rinoceronte', 'marfil', 'cuerno']
 
     def start_requests(self):
-        urls = [self.base_url + keyword +'.htm' for keyword in self.search_keywords]
+        urls = [self.base_url + keyword +'&verticalId=' for keyword in self.search_keywords]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         extraction = Selector(response=response, type='html')\
-            .xpath('//div[re:test(@class, "aditem")]').extract()
+            .xpath('//div[re:test(@class, "container-wall")]').extract()
 
-        with open('test.html', 'a') as f:
+        with open('test_wallapop.html', 'a') as f:
             for el in extraction:
                 f.write(el)
 
