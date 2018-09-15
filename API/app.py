7f19d5ea0e3b46b501fd5b58fa2e82b9b9a86296
Gigi <giselaruzafamado@gmail.com>
2018-09-15 19:42:30 +0200

    Add endpoints for autors

--- API/app.py
+++ API/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, jsonify
 
 from flask_cors import CORS
@@ -5,26 +7,72 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/posts')
 def get_posts():
     return jsonify(
         [
             {
                 'id': 321,
-                'title': 'vendo cocodrilo',
-                'address': 'Inlab B5',
-                'latitude': 41.3884966,
-                'longitude': 41.3884966,
-                'tags': ['marfil', 'tigre', 'cocodrilo']
+                'Title': 'vendo cocodrilo',
+                'Description': 'vendo cocodrilo',
+                'Date:"10/09/2010'
+                'Location': json.dumps({'Latitude': 41.3884966, 'Longitude': 41.3884966}),
+                'Images': 'img',
+                'User': json.dumps(
+                    {'name': 'gonredo', 'email': 'gonredo@cazador.es', 'extra': json.dumps(['extra1','extra2'])}),
+                'Categories': json.dumps(['tigre', 'unicornio']),
+                'Price': 9.300,
+                'URL': 'url'
             },
             {
                 'id': 322,
-                'title': 'vendo unicornio',
-                'address': 'Inlab B5',
-                'latitude': 41.3884966,
-                'longitude': 2.33884966,
-                'tags': ['raro', 'especial', 'salvaje']
+                'Title': 'vendo cocodrilo',
+                'Description': 'vendo cocodrilo',
+                'Date:"10/09/2010'
+                'Location': json.dumps({'Latitude': 41.3884966, 'Longitude': 41.3884966}),
+                'Images': 'img',
+                'User': json.dumps(
+                    {'name': 'juanjo', 'email': 'juanjo@cazador.es', 'extra': json.dumps(['extra1','extra2'])}),
+                'Categories': json.dumps(['tigre']),
+                'Price': 9.300,
+                'URL': 'url'
             }
+
+        ]
+
+    )
+
+
+@app.route('/tags')
+def get_tags():
+    return jsonify(
+        [
+            'marfil, rino', 'unicornio'
+
+        ]
+
+    )
+
+
+@app.route('/users')
+def get_users():
+
+    user1 = {
+        'pepe': 'papo'
+    }
+
+    return jsonify(
+        [
+            {
+                'name': 'gonredo',
+                'email': 'gonredo@cazador.es',
+                'extra': json.dumps('extra2')},
+            {
+                'name': 'juanjo',
+                'email': 'juanjo@cazador.es',
+                'extra': json.dumps('extra1')}
+
         ]
 
     )
