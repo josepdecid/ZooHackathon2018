fce1532ca18883123c378bc9c4eb0cd8123474e3
Pau Torrents <pautg.95@gmail.com>
2018-09-16 11:11:50 +0200

    Added more columns to tags chart

--- web/js/scripts/index.js
+++ web/js/scripts/index.js
@@ -198,7 +198,7 @@
                 return 1;
             return 0;
         });
-        tags = tags.slice(0, 5);
+        tags = tags.slice(0, 7);
 
         var ctx = document.getElementById("tagsChart").getContext('2d');
         var chart = new Chart(ctx, {
@@ -215,7 +215,8 @@
                             'rgba(255, 206, 86, 0.2)',
                             'rgba(75, 192, 192, 0.2)',
                             'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(83, 120, 132, 0.2)'
                         ],
                         borderColor: [
                             'rgba(255,99,132,1)',
@@ -223,7 +224,8 @@
                             'rgba(255, 206, 86, 1)',
                             'rgba(75, 192, 192, 1)',
                             'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(83, 120, 132, 1)'
                         ],
                         borderWidth: 1
                     }
